fbde29d766e4f316bd9f49cae3e3c9a636b7cc70
Rodion Ponomarenko <radion91@gmail.com>
2017-09-11 12:40:08 +0300

    end

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '91e2de6d110b3d5f606b18ded4fbd83f';
-//const ROOT_ULR = `https://crossorigin.me/http://samples.openweathermap.org/data/2.5/weather`
-const ROOT_ULR = `http://samples.openweathermap.org/data/2.5/weather`
+const ROOT_ULR = `http://api.openweathermap.org/data/2.5/forecast`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
@@ -10,9 +9,8 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city){
 
-    const url = `${ROOT_ULR}?q=${city}&appid=${API_KEY}`;
+    const url = `${ROOT_ULR}?q=${city}&appid=${API_KEY}&units=metric&lang=ua`;
     const request = axios.get(url);
-    console.log('request',request)
 
     return {
         type: FETCH_WEATHER,
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
-
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 class WeatherList extends Component {
 
-    renderWeather(cityData){
+    renderWeather(cityData,index){
+
+        const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const preessures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+
+        const {lon,lat} = cityData.city.coord;
+
         return(
-            <tr key={cityData.name}>
-                <td>{cityData.name}</td>
+            <tr key={index}>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
+                <td>
+                    <Chart data={temps} color="orange" units="°C"/>
+                </td>
+                <td>
+                    <Chart data={preessures} color="blue" units="hPa" />
+                </td>
+                <td>
+                    <Chart data={humidities} color="purple" units="%" />
+                </td>
             </tr>
         )
     }
@@ -17,10 +35,10 @@ class WeatherList extends Component {
             <table className="table table-hover">
                 <thead>
                     <tr>
-                        <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Город</th>
+                        <th>Температура (°C)</th>
+                        <th>Давление (hPa)</th>
+                        <th>Влажность (%)</th>
                     </tr>
                 </thead>
                 <tbody>
